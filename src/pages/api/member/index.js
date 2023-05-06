9ed611b0f502b27c68e9d28f76d00724e9889e6b
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-05-06 22:04:17 +0700

    create API GET/POST member

---
+++ src/pages/api/member/index.js
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2"
+
+export default function getMember(req, res){
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'library_management_system' 
+      });
+    
+      
+
+    const getMethod = () => {
+      connection.query("SELECT * FROM member",
+      (err, results, fields) =>{
+
+        if(err){
+          console.log(err)
+          return res.status(400).send({name: "req.query.id"});
+        }
+          return res.status(200).json(results)
+        })
+      }
+
+    const postMethod = () => {
+      const {firstname, midname, lastname, ID, DOB, phone, email, addresses1, addresses2, city, postcode, country} = req.body;
+      
+      connection.query(`INSERT INTO member (member_id, member_created, member_fname, member_mname, member_lname, member_personalID, member_DOB, member_phone, member_email, member_addresses_1, member_addresses_2, member_city, member_postcode, member_country)VALUES (NULL, current_timestamp(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [firstname, midname, lastname, ID, DOB, phone, email, addresses1, addresses2, city, postcode, country],
+        (err, results, fields) =>{
+          if(err){
+            console.log("Error while inserting", err);
+            return res.status(400).send(err);
+          }
+          return res.status(201).json({message: "New user successfuly created!"})
+      })
+    }
+
+    switch(req.method){
+        case 'GET':
+            getMethod();
+        case 'POST':
+            postMethod();
+    }
+
+}
+
