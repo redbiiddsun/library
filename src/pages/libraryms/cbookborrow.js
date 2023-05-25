d78e9532b1812a8f8755c07c2ed83ead1107c4be
NongEs <133220527+NongEs@users.noreply.github.com>
2023-05-26 02:36:00 +0700

    update:cbookborrow js

---
+++ src/pages/libraryms/cbookborrow.js
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import { Outfit } from "next/font/google";
+import Navbar from "../components/navbar";
+import styles from "@/styles/components/cbookborrow.module.css";
+import Link from 'next/link'
+const outfit = Outfit({ subsets: ["latin"] });
+
+function CbookBorrowPage() {
+  return (
+    <>
+
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit&display=swap"
+          rel="stylesheet"
+        />
+        <title>Library Management System</title>
+      </Head>
+      <main>
+        <Navbar/>
+        <span className={styles.currentmem}>Request Book</span>
+        <div className={styles.cmcontainer}> 
+         <div className={styles.inputContainer}>
+                <label htmlFor ="member-id">Member ID :</label>
+                <input type = "text" id = "member-id" placeholder="Member ID" ></input>
+                </div>
+        <div className={styles.inputContainer}>
+                <label htmlFor ="current-date">Current Date :</label>
+                <input type = "text" id = "current-date" placeholder="DD/MM/YYYY" ></input>
+                </div>
+        <div className={styles.inputContainer}>
+                <label htmlFor ="return-date">Return date :</label>
+                <input type = "text" id = "return-date" placeholder="DD/MM/YYYY" ></input>
+                 </div>
+       
+
+
+
+
+
+
+
+
+        <div className={styles.backbox}>
+        <Link href="/libraryms/cbook"><span>Back</span></Link>
+        </div>
+
+        <div className={styles.confirmbox}>
+          <button type="submit" form="member">Confirm</button>
+        </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default CbookBorrowPage;
+
+
