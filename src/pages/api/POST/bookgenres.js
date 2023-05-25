4d506949263011076c8c4a21508346c609a3a983
Intouchhhh <93993444+Intouchhhh@users.noreply.github.com>
2023-05-26 00:02:24 +0700

    add: POST bookgenres

---
+++ src/pages/api/POST/bookgenres.js
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2"
+
+export default function insertMember(req, res){
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME 
+      });
+    
+
+    const postMethod = () => {
+      const {bookID,genresID} = req.body;
+      
+      connection.query(`INSERT INTO bookgenres (book_id,genres_id) VALUES (?, ?)`,
+        [bookID,genresID],
+        (err, results, fields) =>{
+          if(err){
+            console.log("Error while inserting", err);
+            return res.status(400).send(err);
+          }
+          return res.status(201).json({message: "New bookgenres successfuly created!", checkoutID: results.insertId})
+      })
+    }
+
+  if (req.method === 'POST') {
+    postMethod();
+  } else {
+    return res.status(405).json({message: "Method Not Allowed. Use POST only"})
+  }
+
+}
