b28297ce296d276be4128262ec0409ee4682d9d6
Intouchhhh <93993444+Intouchhhh@users.noreply.github.com>
2023-05-25 23:49:40 +0700

    add: POST checkout

---
+++ src/pages/api/POST/checkout.js
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2"
+
+export default function insertMember(req, res){
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME 
+      });
+    
+
+    const postMethod = () => {
+      const {checkoutID,memberID,amount,staffID} = req.body;
+      
+      connection.query(`INSERT INTO checkouts (checkout_id,member_id,checkout_amount,created_checkout,staff_id) VALUES (NULL, ?, ?, current_timestamp(), ?)`,
+        [checkoutID,memberID,amount,staffID],
+        (err, results, fields) =>{
+          if(err){
+            console.log("Error while inserting", err);
+            return res.status(400).send(err);
+          }
+          return res.status(201).json({message: "New checkouts successfuly created!", checkoutID: results.insertId})
+      })
+    }
+
+  if (req.method === 'POST') {
+    postMethod();
+  } else {
+    return res.status(405).json({message: "Method Not Allowed. Use POST only"})
+  }
+
+}
