33cee6a808f1a9db1dccef556050323996ffa378
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-05-26 15:52:04 +0700

    add: dialog component

---
+++ src/pages/components/dialog.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+export function UseDialog() {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickToOpen = () => {
+        setOpen(true);
+    };
+
+    const handleToClose = () => {
+            setOpen(false);
+    };
+
+
+    function DialogComponent({ title, content }) {
+        
+        return (
+            <Dialog open={open} onClose={handleToClose}>
+                <DialogTitle>{title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {content}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleToClose}
+                        color="primary" autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+        );
+    }
+
+    return {handleClickToOpen,DialogComponent};
+}
