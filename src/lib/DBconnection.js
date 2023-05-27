874c1fe67df6a1dce9cc4a0e4b1568a9428e0627
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-05-27 20:24:15 +0700

    add: connectionLimit

--- src/lib/DBconnection.js
+++ src/lib/DBconnection.js
@@ -6,7 +6,8 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
+    connectionLimit: 10,
+    maxIdle: 10,
 });
 
 export default connection;
