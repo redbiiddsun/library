9fc012a01046d061dab0ff7c71e2899d98f8848d
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-05-28 23:16:30 +0700

    add: API for tableComponent

--- src/pages/api/GET/analytic/bookdaily/index.js
+++ src/pages/api/GET/analytic/bookdaily/index.js
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import mysql from "mysql2"
 import connection from "@/lib/DBconnection";
+
 export default function getBookDaily(req, res){
 const getMethod = () => {
         connection.query("SELECT book_id, book_title, ISBN, `language`, author_first_name,author_last_name, publisher, bookcategory FROM books b,publishers p,authors a,bookcategory bc,`language` l WHERE b.publisher_id = p.publisher_id AND b.author_id = a.author_id AND b.bookcategory_id = bc.bookcategory_id AND b.language_id = l.language_id AND DATE(b.book_created) = CURDATE();",
--- src/pages/api/GET/analytic/bookrequestdaily/index.js
+++ src/pages/api/GET/analytic/bookrequestdaily/index.js
@@ -7,13 +7,13 @@ export default function getBookRequest(req, res){
     
     // GET Author 
     const getMethod = () => {
-        connection.query(`SELECT rb.request_id, CONCAT(m.member_fname," " ,m.member_lname) AS Member, rd.request_title, l.language, CONCAT(a.author_first_name," " ,a.author_last_name) AS Author,p.publisher FROM requestdetail rd, requestbooks rb , language l, authors a, publishers p, member m WHERE rd.request_id = rb.request_id AND rb.member_id = m.member_id AND rd.language_id = l.language_id AND rd.author_id = a.author_id AND rd.publisher_id = p.publisher_id`,
+        connection.query(`SELECT rb.request_id, CONCAT(m.member_fname," " ,m.member_lname) AS Member, rd.request_title, l.language, CONCAT(a.author_first_name," " ,a.author_last_name) AS Author,p.publisher FROM requestdetail rd, requestbooks rb , language l, authors a, publishers p, member m WHERE rd.request_id = rb.request_id AND rb.member_id = m.member_id AND rd.language_id = l.language_id AND rd.author_id = a.author_id AND rd.publisher_id = p.publisher_id AND DATE(rb.requestbook_created) = CURDATE()`,
         (err, results, fields) =>{ 
 
             if(results.length != 0 && !err){
                 return res.status(200).json(results);
             }else{
-                return res.status(400).json({error: "Invalid ID"});
+                return res.status(400).json({error: "No data"});
             }
         })
     }
