dbccd76dcc3966f15b43e54b8e403a7bab9b54a7
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-05-28 10:21:37 +0700

    add: GET bookgenres with ID

---
+++ src/pages/api/GET/bookgenres/[id].js
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2"
+import connection from "../../../../lib/DBconnection"
+
+export default function getBookgenresById(req, res){
+
+// GET Member with MemberID 
+    const getMethod = () => {
+        connection.query("SELECT * FROM bookgenres WHERE book_id = ?", [req.query.id],
+        (err, results, fields) =>{ 
+
+            if(results.length != 0 && !err){
+                return res.status(200).json(results);
+            }else{
+                return res.status(400).json({error: "Invalid ID"});
+            }
+        })
+    }
+
+
+    if (req.method === 'GET') {
+        getMethod();
+    } else {
+        return res.status(405).json({message: "Method Not Allowed. Use GET only"})
+    }
+    
+}
+
