371d6d81a03eb12287d3f5c06ad99c685ff75044
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-05-28 15:02:32 +0700

    change: DB connection type

--- src/pages/api/PATCH/staff.js
+++ src/pages/api/PATCH/staff.js
@@ -3,18 +3,11 @@ import mysql from "mysql2"
 import connection from "../../../lib/DBconnection"
 
 export default function updateStaff(req, res){
-    const connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME 
-      });
-    
 
     const postMethod = () => {
       const {staff_id, firstname, midname, lastname, NationID, DOB, phone, email, addresses1, addresses2, city, postcode, country, bankname, banknum} = req.body;
       connection.query(`UPDATE staff SET staff_fname = ?, staff_mname=?, staff_lname=?, 
-      staff_personalID=?, staff_DOB=?, staff_phone=?, staff_email=?, staff_addresses_1=?, staff_addresses_2=?, staff_city=?, staff_postcode=?,
+      staff_personalID=?, staff_DOB=?, staff_phone=?, staff_email=?, staff_addresse_1=?, staff_addresse_2=?, staff_city=?, staff_postcode=?, staff_country=?,
       bank_name=?, bankAcc_num=? WHERE staff_id =?`,
         [firstname, midname, lastname, NationID, DOB, phone, email, addresses1, addresses2, city, postcode, country, bankname, banknum, staff_id],
         (err, results, fields) =>{
@@ -22,7 +15,7 @@ export default function updateStaff(req, res){
             console.log("Error while inserting", err);
             return res.status(400).send(err);
           }
-          return res.status(201).json({message: "staff successfuly updated!", memberID: member_id})
+          return res.status(201).json({ message: "staff successfuly updated!", RowAffected: results.affectedRows })
       })
     }
 
