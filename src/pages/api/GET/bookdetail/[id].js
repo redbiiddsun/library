d75270e169d934c6096aadff1d4bb4be19b52b60
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-05-29 20:36:41 +0700

    fix: SQL result wrong

--- src/pages/api/GET/bookdetail/[id].js
+++ src/pages/api/GET/bookdetail/[id].js
@@ -6,7 +6,7 @@ export default function getฺBookById(req, res){
 
 // GET Member with MemberID 
     const getMethod = () => {
-        connection.query(`SELECT b.book_id, b.book_title,b.ISBN, b.page, l.language, CONCAT(a.author_first_name, " ", a.author_last_name), p.publisher, b.publication_year, c.bookcategory FROM books b, language l, authors a, publishers p, bookcategory c WHERE b.language_id = l.language_id AND b.author_id = a.author_id AND b.publisher_id = p.publisher_id AND b.bookcategory_id = c.bookcategory_id AND book_id= ?`, [req.query.id],
+        connection.query(`SELECT b.book_id, b.book_title,b.ISBN, b.page, l.language, CONCAT(a.author_first_name, " ", a.author_last_name) As author, p.publisher, b.publication_year, c.bookcategory FROM books b, language l, authors a, publishers p, bookcategory c WHERE b.language_id = l.language_id AND b.author_id = a.author_id AND b.publisher_id = p.publisher_id AND b.bookcategory_id = c.bookcategory_id AND book_id= ?`, [req.query.id],
         (err, results, fields) =>{ 
 
             if(results.length != 0 && !err){
