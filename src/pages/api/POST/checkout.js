edf748ceb75d1a316729bbf0240cd1df0e0586c7
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-05-29 20:37:34 +0700

    add: checkout API for Borrow page

--- src/pages/api/POST/checkout.js
+++ src/pages/api/POST/checkout.js
@@ -3,26 +3,45 @@ import mysql from "mysql2"
 import connection from "../../../lib/DBconnection"
 
 
-export default function insertCheckout(req, res){
-    
-    const postMethod = () => {
-      const {checkoutID,memberID,amount,staffID} = req.body;
-      
-      connection.query(`INSERT INTO checkouts (checkout_id,member_id,checkout_amount,created_checkout,staff_id) VALUES (NULL, ?, ?, current_timestamp(), ?)`,
-        [checkoutID,memberID,amount,staffID],
-        (err, results, fields) =>{
-          if(err){
-            console.log("Error while inserting", err);
-            return res.status(400).send(err);
+export default function insertCheckout(req, res) {
+
+  const postMethod = () => {
+
+    let checkoutID = 0;
+
+    const { bookList, memberID, amount, staffID, currentdate, returndate } = req.body;
+
+    connection.query(`INSERT INTO checkouts (checkout_id, member_id, checkout_amount, created_checkout, staff_id) VALUES (NULL, ?, ?, current_timestamp(), ?)`,
+      [memberID, amount, staffID],
+      (err, results, fields) => {
+        if (err) {
+          console.log("Error while inserting", err);
+          return res.status(400).send(err);
+        } else {
+
+          checkoutID = results.insertId
+
+          for (let i = 0; i < bookList.length; i++) {
+            //console.log(bookList[i].book_id);
+            connection.query(`INSERT INTO checkoutsdetail (checkout_id, book_id, checkout_date, return_duration_date, is_returned) VALUES (?, ?, ?, ?, 0)`,
+              [checkoutID, bookList[i].book_id, currentdate, returndate],
+              (err, results, fields) => {
+                if (err) {
+                  console.log("Error while inserting", err);
+                  return res.status(400).send(err);
+                }
+                return res.status(201).json({ message: "New checkouts successfuly created!", checkoutID: checkoutID })
+              })
           }
-          return res.status(201).json({message: "New checkouts successfuly created!", checkoutID: results.insertId})
+        }
+
       })
-    }
+  }
 
   if (req.method === 'POST') {
     postMethod();
   } else {
-    return res.status(405).json({message: "Method Not Allowed. Use POST only"})
+    return res.status(405).json({ message: "Method Not Allowed. Use POST only" })
   }
 
 }
